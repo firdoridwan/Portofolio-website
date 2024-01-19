933392ec2ec5048027919e427c003074a28a6979
Firdo Ridwan <115608877+firdoridwan@users.noreply.github.com>
2024-01-19 21:24:22 +0700

    Add files via upload

---
+++ video.js
@@ -0,0 +1,26 @@
+// Video Gallery
+let listVideo = document.querySelectorAll('.video-list .vid');
+let mainVideo = document.querySelector('.main-video video');
+let title = document.querySelector('.main-video .title');
+
+listVideo.forEach(video => {
+    video.onclick = () => {
+        listVideo.forEach(vid => vid.classList.remove('active'));
+        video.classList.add('active');
+        if(video.classList.contains('active')) {
+            let src = video.children[0].getAttribute('src');
+            mainVideo.src = src;
+            let text = video.children[1].innerHTML;
+            title.innerHTML = text;
+        }
+    };
+});
+
+// Text Animation
+var typingEffect = new Typed(".multiText", {
+    strings : ["Portfolio", "Gallery",],
+    loop : true,
+    typeSpeed : 100,
+    backSpeed : 80,
+    backDelay : 1500,
+})
